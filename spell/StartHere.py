83f7ee23578cce87b9a62292157962127674ac9d
diablo1029 <diablo1029@DESKTOP-T501OUU.Home>
2018-01-29 06:24:53 -0700

    1/29/2018

--- spell/StartHere.py
+++ spell/StartHere.py
@@ -1,12 +1,12 @@
-
+playerHp = 100
 firePoints_names = ["infernal", "blaze", "flame", "ember", ""]
 waterPoints_names = ["glacial", "blizzard", "ice", "hail", ""]
 earthPoints_names = ["canyon", "chasm", "trench", "cracks", ""]
 airPoints_names = ["mountain", "fortress", "wall", "mound", ""]
 darkPoints_names = ["consuming", "draining", "leeching", "shrinking", ""]
 lightPoints_names = ["exploding", "erupting", "blasting", "bursting", ""]
-totalPoints = 60
-usePoints = 60
+totalPoints = 30
+usePoints = 30
 name = input("Hello mage, what is your name?")
 print ("Welcome to the spell generator", name, ", in this you are alloted a\nmaximum of 60 points to put into 6 base elements\nfire, water, earth, air, dark, light\nbut each element is only allowed a maximum of 16 points")
 
@@ -190,22 +190,112 @@ if (int(fire) == 16) and (int(dark) == 16):
     print ("You have unlocked the magic element 'Fear'.\nFear can only be accessed when your fire and dark base elements are 16.")
 if (int(fire) == 16) and (int(air) == 16):
     print ("You have unlocked the magic element 'Heat'.\nHeat can only be accessed when your fire and air base elements are 16.")
-
+if (int(fire) >= 9):
+    fireDmg = (int(fire)%2)+2
+else:
+    fireDmg = 0
+if (int(water) >= 9):
+    waterDmg = (int(water)%2)+2
+else:
+    waterDmg = 0
+if (int(earth) >= 9):
+    earthDmg = (int(earth)%2)+2
+else:
+    earthDmg = 0
+if (int(air) >= 9):
+    airDmg = (int(air)%2)+2
+else:
+    airDmg = 0
+if (int(dark) >= 9):
+    darkDmg = (int(dark)%2)+2
+else:
+    darkDmg = 0
+if (int(light) >= 9):
+    lightDmg = (int(light)%2)+2
+else:
+    lightDmg = 0
+playerDmg = fireDmg+waterDmg+earthDmg+airDmg+darkDmg+lightDmg
+playerLvl = 1
+playerXp = 0
+nextXp = 100
+while playerXp >= nextXp :
+    playerLvl += 1
+    playerXp = playerXp - nextXp
+    nextXp = round(nextXp * 1.5)
 ready = input("Are you ready to start your adventure y/n?")
+enemyHp = 0
+enemyXp = 0
+enemyDmg = 0
+def enemy():
+    global enemyType
+    global enemyHp
+    global enemyXp
+    global enemyDmg
+    print (enemyType, "\n", "Enemy Hp = ", enemyHp, "\n", "Enemy Damage = ", enemyDmg)
+    if enemyType == "Orc":
+        enemyHp = 150
+        enemyXp = round(enemyHp*2)
+        enemyDmg = round(enemyHp/5)
+    if enemyType == "Bandit":
+        enemyHp = 75
+        enemyXp = round(enemyHp*2)
+        enemyDmg = round(enemyHp/5)
+    if enemyType == "Dragon":
+        enemyHp = 1000
+        enemyXp = round(enemyHp*2)
+        enemyDmg = round(enemyHp/5)
+    if enemyType == "Wraith":
+        enemyHp = 250
+        enemyXp = round(enemyHp*2)
+        enemyDmg = round(enemyHp/5)
+def battle():
+    global enemyHp
+    global enemyXp
+    global enemyDmg
+    while enemyHp > 0:
+        battle = input("do you wish to attack, block, or run?")
+        if battle == "attack":
+            enemyHp = enemyHp - round(playerDmg/1.5)
+            print ("The orc lashes out in anger")
+            playerHp = playerHp - enemyDmg
+            print (playerHp)
+            if enemyHp <= 0:
+                print: "You have won the battle!"
+                playerXp = playerXp + enemyXp
+        if battle == "block":
+            playerHp = playerHp - round(enemyDmg/1.5)
+            print ("With the orc leaving itself open you hit it in retaliation")
+            enemyHp = enemyHp - playerDmg
+        if battle == "run":
+            print ("You successfully run away")
 def adventure():
+    enemyType = ""
     print ("You walk out of the master mages house and are briefly blinded by the warm suns light.")
     direction = input("As your vision returns you see that you are standing\nat the edge a road running left to right which way would you like to go?")
-    if (direction == "left") or (direction == "Left") or (direction == "L") or (direction == "l"):
-        print ("As you continue down the road you eventually run across a pair of bandits \n do you wish to help the woman or look the other way")
+    if direction == "l" or direction == "Left":
+        engage = input("As you continue down the road you eventually run across a pair of bandits robbing a woman.\n do you wish to help the woman?  y/n")
+        if engage == "y" or engage == "yes":
+            enemyType = "Bandit"
+            enemy()
+            battle()
+        else:
+            print ("You walk past the woman an bandits feeling her gaze hopelessly stick to your ever shinking back")
     else :
-        print ("As you continue down the road you eventually run across a camp of Orcs ")
+        engage = input("As you continue down the road you eventually run across a Orc\nDo you wish to engage the Orc?  y/n")
+        if engage == "y" or engage == "yes":
+            enemyType = "Orc"
+            enemy()
+            battle()
+        else :
+            turn = input("You sneak around the orcs and continue down the road until you come to a crossing with another street\nWould you like to turn onto the other road?  y/n")
+            if turn == "y" or turn == "yes":       
+                print ("You turn down the intersecting road until you come a cross a town")
 if ready == "y":
     print ("Alright be on your way then.")
     adventure()
-    
-    
-    
-    
+print ("Level: ", playerLvl)
+print ("Exp: ", playerXp)
+print ("Next Level: ", nextXp)
 
 
 
