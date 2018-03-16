b7f939f064ea00f258d8caeb19ae4b2414d2bcea
diablo1029 <diablo1029@DESKTOP-T501OUU.Home>
2018-03-16 01:09:00 -0600

    3/16/2018 1:08 AM

--- spell/StartHere.py
+++ spell/StartHere.py
@@ -1,3 +1,4 @@
+from builtins import int
 playerHp = 100
 firePoints_names = ["infernal", "blaze", "flame", "ember", ""]
 waterPoints_names = ["glacial", "blizzard", "ice", "hail", ""]
@@ -5,25 +6,29 @@ earthPoints_names = ["canyon", "chasm", "trench", "cracks", ""]
 airPoints_names = ["mountain", "fortress", "wall", "mound", ""]
 darkPoints_names = ["consuming", "draining", "leeching", "shrinking", ""]
 lightPoints_names = ["exploding", "erupting", "blasting", "bursting", ""]
-totalPoints = 30
-usePoints = 30
+totalPoints = 48
+usePoints = 48
 name = input("Hello mage, what is your name?")
 print ("Welcome to the spell generator", name, ", in this you are alloted a\nmaximum of 60 points to put into 6 base elements\nfire, water, earth, air, dark, light\nbut each element is only allowed a maximum of 16 points")
 
 fire = input("How many points would you like to allot to the fire element?")
 
+if (fire == ""):
+    print ("Please reenter your points and remember that an input above 16 is not allowed")
+    fire = input("How many points would you like to allot to the fire element?")
 while (int(fire) < 0) or (int(fire) > 16):
     print ("Please reenter your points and remember that an input above 16 is not allowed")
     fire = input("How many points would you like to allot to the fire element?")
 else:
     usePoints = usePoints - int(fire)
     while usePoints < 0:
-        print ("Please reenter your points because you exceeded the total alottable points")
+        print ("Please reenter your points because you exceeded the total allottable points")
         usePoints = usePoints + int(fire)
         print (usePoints, "unallocated points")
         fire = input("How many points would you like to allot to the fire element?")
         usePoints = usePoints - int(fire)
-        
+
+            
 print (usePoints, "unallocated points")
 
 if (int(fire) == 9) or (int(fire) == 10):
@@ -39,17 +44,21 @@ elif int(fire) < 9:
     
 water = input("How many points would you like to allot to the water element?")
 
+if (water == ""):
+    print ("Please reenter your points and remember that an input above 16 is not allowed")
+    water = input("How many points would you like to allot to the water element?")
 while (int(water) < 0) or (int(water) > 16):
     print ("Please reenter your points and remember that an input above 16 is not allowed")
     water = input("How many points would you like to allot to the water element?")
 else:
     usePoints = usePoints - int(water)
     while usePoints < 0:
-        print ("Please reenter your points because you exceeded the total alottable points")
+        print ("Please reenter your points because you exceeded the total allottable points")
         usePoints = usePoints + int(water)
         print (usePoints, "unallocated points")
         water = input("How many points would you like to allot to the water element?")
         usePoints = usePoints - int(water)
+
         
 print (usePoints, "unallocated points")
 
@@ -66,18 +75,22 @@ elif int(water) < 9:
     
 earth = input("How many points would you like to allot to the earth element?")
 
+if (earth == ""):
+    print ("Please reenter your points and remember that an input above 16 is not allowed")
+    earth = input("How many points would you like to allot to the earth element?")
 while (int(earth) < 0) or (int(earth) > 16):
     print ("Please reenter your points and remember that an input above 16 is not allowed")
     earth = input("How many points would you like to allot to the earth element?")
 else:
     usePoints = usePoints - int(earth)
     while usePoints < 0:
-        print ("Please reenter your points because you exceeded the total alottable points")
+        print ("Please reenter your points because you exceeded the total allottable points")
         usePoints = usePoints + int(earth)
         print (usePoints, "unallocated points")
         earth = input("How many points would you like to allot to the earth element?")
         usePoints = usePoints - int(earth)
-        
+
+
 print (usePoints, "unallocated points")
 
 if (int(earth) == 9) or (int(earth) == 10):
@@ -93,6 +106,9 @@ elif int(earth) < 9:
     
 air = input("How many points would you like to allot to the air element?")
 
+if (air == ""):
+    print ("Please reenter your points and remember that an input above 16 is not allowed")
+    air = input("How many points would you like to allot to the air element?")
 while (int(air) < 0) or (int(air) > 16):
     print ("Please reenter your points and remember that an input above 16 is not allowed")
     air = input("How many points would you like to allot to the air element?")
@@ -104,6 +120,7 @@ else:
         print (usePoints, "unallocated points")
         air = input("How many points would you like to allot to the air element?")
         usePoints = usePoints - int(air)
+
         
 print (usePoints, "unallocated points")
 
@@ -120,6 +137,9 @@ elif int(air) < 9:
     
 dark = input("How many points would you like to allot to the dark element?")
 
+if (dark == ""):
+    print ("Please reenter your points and remember that an input above 16 is not allowed")
+    dark = input("How many points would you like to allot to the dark element?")
 while (int(dark) < 0) or (int(dark) > 16):
     print ("Please reenter your points and remember that an input above 16 is not allowed")
     dark = input("How many points would you like to allot to the dark element?")
@@ -131,6 +151,7 @@ else:
         print (usePoints, "unallocated points")
         dark = input("How many points would you like to allot to the dark element?")
         usePoints = usePoints - int(dark)
+
         
 print (usePoints, "unallocated points")
 
@@ -147,6 +168,9 @@ elif int(dark) < 9:
     
 light = input("How many points would you like to allot to the light element?")
 
+if (light == ""):
+    print ("Please reenter your points and remember that an input above 16 is not allowed")
+    light = input("How many points would you like to allot to the light element?")
 while (int(light) < 0) or (int(light) > 16):
     print ("Please reenter your points and remember that an input above 16 is not allowed")
     light = input("How many points would you like to allot to the light element?")
@@ -158,6 +182,7 @@ else:
         print (usePoints, "unallocated points")
         light = input("How many points would you like to allot to the light element?")
         usePoints = usePoints - int(light)
+
         
 print (usePoints, "unallocated points")
 
@@ -191,102 +216,125 @@ if (int(fire) == 16) and (int(dark) == 16):
 if (int(fire) == 16) and (int(air) == 16):
     print ("You have unlocked the magic element 'Heat'.\nHeat can only be accessed when your fire and air base elements are 16.")
 if (int(fire) >= 9):
-    fireDmg = (int(fire)%2)+2
+    fireDmg = (int(fire)/2)+2
 else:
     fireDmg = 0
 if (int(water) >= 9):
-    waterDmg = (int(water)%2)+2
+    waterDmg = (int(water)/2)+2
 else:
     waterDmg = 0
 if (int(earth) >= 9):
-    earthDmg = (int(earth)%2)+2
+    earthDmg = (int(earth)/2)+2
 else:
     earthDmg = 0
 if (int(air) >= 9):
-    airDmg = (int(air)%2)+2
+    airDmg = (int(air)/2)+2
 else:
     airDmg = 0
 if (int(dark) >= 9):
-    darkDmg = (int(dark)%2)+2
+    darkDmg = (int(dark)/2)+2
 else:
     darkDmg = 0
 if (int(light) >= 9):
-    lightDmg = (int(light)%2)+2
+    lightDmg = (int(light)/2)+2
 else:
     lightDmg = 0
-playerDmg = fireDmg+waterDmg+earthDmg+airDmg+darkDmg+lightDmg
+print(fireDmg, waterDmg, earthDmg, airDmg, darkDmg, lightDmg)
+playerDmg = 10*(round(fireDmg+waterDmg+earthDmg+airDmg+darkDmg+lightDmg))
 playerLvl = 1
 playerXp = 0
 nextXp = 100
-while playerXp >= nextXp :
-    playerLvl += 1
-    playerXp = playerXp - nextXp
-    nextXp = round(nextXp * 1.5)
 ready = input("Are you ready to start your adventure y/n?")
+enemyType = "Dragon"
 enemyHp = 0
 enemyXp = 0
 enemyDmg = 0
+def xp():
+    global nextXp
+    global playerXp
+    global playerLvl
+    while playerXp >= nextXp :
+        playerXp = playerXp - nextXp
+        nextXp = round(nextXp * 1.5)
+        playerLvl += 1
 def enemy():
     global enemyType
     global enemyHp
     global enemyXp
     global enemyDmg
-    print (enemyType, "\n", "Enemy Hp = ", enemyHp, "\n", "Enemy Damage = ", enemyDmg)
     if enemyType == "Orc":
         enemyHp = 150
         enemyXp = round(enemyHp*2)
         enemyDmg = round(enemyHp/5)
+        print (enemyType, "\n", "Enemy Hp = ", enemyHp, "\n", "Enemy Damage = ", enemyDmg)
     if enemyType == "Bandit":
         enemyHp = 75
         enemyXp = round(enemyHp*2)
         enemyDmg = round(enemyHp/5)
+        print (enemyType, "\n", "Enemy Hp = ", enemyHp, "\n", "Enemy Damage = ", enemyDmg)
     if enemyType == "Dragon":
         enemyHp = 1000
         enemyXp = round(enemyHp*2)
         enemyDmg = round(enemyHp/5)
+        print (enemyType, "\n", "Enemy Hp = ", enemyHp, "\n", "Enemy Damage = ", enemyDmg)
     if enemyType == "Wraith":
         enemyHp = 250
         enemyXp = round(enemyHp*2)
         enemyDmg = round(enemyHp/5)
+        print (enemyType, "\n", "Enemy Hp = ", enemyHp, "\n", "Enemy Damage = ", enemyDmg)
 def battle():
     global enemyHp
     global enemyXp
     global enemyDmg
+    global enemyType
+    global playerHp
+    global playerDmg
+    global playerXp
+    print ("You engage the", enemyType)
     while enemyHp > 0:
         battle = input("do you wish to attack, block, or run?")
         if battle == "attack":
             enemyHp = enemyHp - round(playerDmg/1.5)
-            print ("The orc lashes out in anger")
+            print(playerDmg)
+            print ("The ", enemyType, "lashes out in anger")
             playerHp = playerHp - enemyDmg
-            print (playerHp)
+            print ("HP:", playerHp, "\n", "Enemy HP:", enemyHp)
             if enemyHp <= 0:
-                print: "You have won the battle!"
+                print ("You have won the battle!")
                 playerXp = playerXp + enemyXp
+                xp()
         if battle == "block":
             playerHp = playerHp - round(enemyDmg/1.5)
-            print ("With the orc leaving itself open you hit it in retaliation")
+            print ("With the", enemyType, "leaving itself open you hit it in retaliation")
             enemyHp = enemyHp - playerDmg
+            print ("HP:", playerHp, "\n", "Enemy HP:", enemyHp)
+            if enemyHp <= 0:
+                print ("You have won the battle!")
+                playerXp = playerXp + enemyXp
+                xp()
         if battle == "run":
             print ("You successfully run away")
+    else:
+        print("You have won the battle!")
 def adventure():
     enemyType = ""
     print ("You walk out of the master mages house and are briefly blinded by the warm suns light.")
     direction = input("As your vision returns you see that you are standing\nat the edge a road running left to right which way would you like to go?")
-    if direction == "l" or direction == "Left":
+    if direction == "l" or direction == "Left" or direction == "left":
         engage = input("As you continue down the road you eventually run across a pair of bandits robbing a woman.\n do you wish to help the woman?  y/n")
         if engage == "y" or engage == "yes":
             enemyType = "Bandit"
             enemy()
             battle()
-        else:
+        if engage == "n" or engage == "no":
             print ("You walk past the woman an bandits feeling her gaze hopelessly stick to your ever shinking back")
-    else :
+    if direction == "r" or direction == "Right" or direction == "right":
         engage = input("As you continue down the road you eventually run across a Orc\nDo you wish to engage the Orc?  y/n")
         if engage == "y" or engage == "yes":
             enemyType = "Orc"
             enemy()
             battle()
-        else :
+        if engage == "n" or engage == "no":
             turn = input("You sneak around the orcs and continue down the road until you come to a crossing with another street\nWould you like to turn onto the other road?  y/n")
             if turn == "y" or turn == "yes":       
                 print ("You turn down the intersecting road until you come a cross a town")
